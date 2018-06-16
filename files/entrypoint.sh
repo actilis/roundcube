3a65296a4a8fa0ecb92d55c51340ad95dff278e6
François MICAUX <fmicaux_at_actilis_._net>
2018-06-16 15:34:53 +0200

    Remove DB table prefix, causes errors with calendar

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -15,9 +15,8 @@ genpasswd() {
 # Database
 export DATABASE_HOST=${DATABASE_HOST:-db}
 export DATABASE_USER=${DATABASE_USER:-root}
+export DATABASE_PASS=${DATABASE_PASS:-secret}
 export DATABASE_NAME=${DATABASE_NAME:-roundcube}
-# (bad) default is root password of the DB linked container
-export DATABASE_PASS=${DATABASE_PASS:-${DB_ENV_MYSQL_ROOT_PASSWORD}}
 
 # Default IMAP/SMTP values
 export IMAP_SERVER=${IMAP_SERVER:-imap}
@@ -32,9 +31,6 @@ export MPM_MINSPARE=${MPM_MINSPARE:-5}
 export MPM_MAXSPARE=${MPM_MAXSPARE:-10}
 export MPM_MAXWORKERS=${MPM_MAXWORKERS:-150}
 export MPM_MAXCONNECTIONS=${MPM_MAXCONNECTIONS:-0}
-
-# Appel à bootstrap.php Créer la base si elle n'existe pas
-php /bootstrap.php
 
 # Roundcube vars
 export DES_KEY=$(genpasswd 24)
@@ -58,5 +54,8 @@ rm -f /var/www/html/config/config.inc.tmpl
 
 chgrp           www-data /var/www/html/config/{defaults,config}.inc.php 
 chmod    440             /var/www/html/config/{defaults,config}.inc.php
+
+# Appel à bootstrap.php Créer la base si elle n'existe pas
+php /bootstrap.php
 
 exec $@
